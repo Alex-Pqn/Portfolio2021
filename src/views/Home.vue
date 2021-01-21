539d3844335650223f0929e6ecaf32ad89169b8d
Alexandre P <@paquienalexandre0@gmail.com>
2021-01-21 09:05:26 +0100

    import headerComponent + display fixed header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,93 +1,58 @@
 <template>
-  <!-- header -->
-  <header>
-    <p>
-      <a href="">
-        ALEXANDRE
-        <br />
-        PAQUIEN<span>.fr</span>
-      </a>
-    </p>
-    <!-- nav -->
-    <nav>
-      <ul>
-        <li>
-          <a href="#about">
-            À PROPOS
-          </a>
-        </li>
-        <li>
-          <a href="">
-            PROJETS
-          </a>
-        </li>
-        <li>
-          <a href="">
-            C.V
-          </a>
-        </li>
-        <li>
-          <a href="">
-            CONTACT
-          </a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  <!-- presentation part -->
-  <presentationComponent />
+  <headerComponent
+    :backgroundColor="'rgb(242, 242, 242)'"
+    :textColor="'black'"
+    :textSpanColor="'rgb(133, 58, 163)'"
+    :opacity="1"
+    :width="'100%'"
+    :position="'relative'"
+    id="header-relative"
+  />
+  <headerComponent
+    :backgroundColor="'white'"
+    :textColor="''"
+    :textSpanColor="''"
+    :opacity="0"
+    :width="'60%'"
+    :position="'fixed'"
+    id="header-fixed"
+  />
+  <!-- introduction part -->
+  <introductionComponent />
 </template>
 
 <script>
-import presentationComponent from "@/components/Home/presentation.vue";
+import introductionComponent from "@/components/Home/introduction.vue";
+import headerComponent from "@/components/Home/header.vue";
 
 export default {
   name: "Home",
+  mounted() {
+    window.onscroll = function() {
+      displayFixedHeader();
+    };
+
+    const headerDisplayDistance = 200;
+    const headerElement = document.querySelector("#header-fixed");
+
+    function displayFixedHeader() {
+      if (
+        document.body.scrollTop >= headerDisplayDistance ||
+        document.documentElement.scrollTop >= headerDisplayDistance
+      ) {
+        headerElement.style.opacity = 1;
+        headerElement.style.width = "100%";
+      } else {
+        headerElement.style.opacity = 0;
+        headerElement.style.width = "60%";
+      }
+    }
+  },
   components: {
-    presentationComponent
+    introductionComponent,
+    headerComponent
   }
 };
 </script>
 
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
-
-// header
-header {
-  display: flex;
-  position: fixed;
-  z-index: 999;
-  width: 100%;
-  p {
-    display: flex;
-    text-align: start;
-    width: 50%;
-    margin: 30px 0 0 35px;
-    letter-spacing: 3px;
-    font-size: 1.25em;
-    a {
-      padding: 7px 10px;
-      line-height: 28px;
-    }
-  }
-  // nav
-  nav {
-    display: flex;
-    justify-content: flex-end;
-    font-family: "Amatic SC", cursive;
-    width: 50%;
-    ul {
-      display: flex;
-      margin: 40px 120px 0 0;
-      li {
-        margin-right: 23px;
-        font-size: 1.45em;
-        letter-spacing: 1px;
-        a {
-          padding-bottom: 5px;
-        }
-      }
-    }
-  }
-}
-</style>
+<style lang="scss"></style>

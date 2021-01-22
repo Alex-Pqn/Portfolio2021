5561ffd610c5773e6af16c28179ebac073fedb70
Alexandre P <@paquienalexandre0@gmail.com>
2021-01-23 00:24:42 +0100

    header js logic moved into header component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,8 @@
 <template>
-  <headerComponent
-    :backgroundColor="'rgb(242, 242, 242)'"
-    :textColor="'black'"
-    :textSpanColor="'rgb(133, 58, 163)'"
-    :opacity="1"
-    :width="'100%'"
-    :position="'relative'"
-    id="header-relative"
-  />
-  <headerComponent
-    :backgroundColor="'white'"
-    :textColor="''"
-    :textSpanColor="''"
-    :opacity="0"
-    :width="'60%'"
-    :position="'fixed'"
-    id="header-fixed"
-  />
+  <!-- headers -->
+  <headerComponent :id="'header-presentation'" />
+  <headerComponent :id="'header-fixed'" />
+
   <!-- introduction part -->
   <introductionComponent />
 </template>
@@ -27,27 +13,6 @@ import headerComponent from "@/components/Home/header.vue";
 
 export default {
   name: "Home",
-  mounted() {
-    window.onscroll = function() {
-      displayFixedHeader();
-    };
-
-    const headerDisplayDistance = 200;
-    const headerElement = document.querySelector("#header-fixed");
-
-    function displayFixedHeader() {
-      if (
-        document.body.scrollTop >= headerDisplayDistance ||
-        document.documentElement.scrollTop >= headerDisplayDistance
-      ) {
-        headerElement.style.opacity = 1;
-        headerElement.style.width = "100%";
-      } else {
-        headerElement.style.opacity = 0;
-        headerElement.style.width = "60%";
-      }
-    }
-  },
   components: {
     introductionComponent,
     headerComponent

858ab80d0c6fcebd66980c116772234facd7595d
Alexandre P <@paquienalexandre0@gmail.com>
2021-01-27 12:58:31 +0100

    import about component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,22 @@
   <!-- introduction part -->
   <headerComponent :id="'header-introduction'" />
   <introductionComponent />
+
+  <!-- about part -->
+  <aboutComponent />
 </template>
 
 <script>
 import introductionComponent from "@/components/Home/introduction.vue";
 import headerComponent from "@/components/Home/header.vue";
+import aboutComponent from "@/components/Home/about.vue";
 
 export default {
   name: "Home",
   components: {
     introductionComponent,
-    headerComponent
+    headerComponent,
+    aboutComponent
   }
 };
 </script>

6a079e255faedf6b918612a253271cf2b602d5aa
Alexandre P <@paquienalexandre0@gmail.com>
2021-02-02 13:09:10 +0100

    AOS animations

--- src/components/about.vue
+++ src/components/about.vue
@@ -2,7 +2,13 @@
   <section id="about" class="about-container">
     <!-- profile pic -->
     <div class="profile-pic">
-      <img src="@/assets/testimg.png" alt="" />
+      <img
+        src="@/assets/testimg.png"
+        alt=""
+        data-aos="fade-up-left"
+        data-aos-duration="600"
+        data-aos-offset="300"
+      />
     </div>
 
     <!-- presentation -->
@@ -15,6 +21,9 @@
         data-icon="quote-left"
         role="img"
         viewBox="0 0 512 512"
+        data-aos="fade-up"
+        data-aos-duration="600"
+        data-aos-offset="1250"
       >
         <path
           d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
@@ -22,7 +31,13 @@
       </svg>
 
       <!-- text -->
-      <div class="presentation__text">
+      <div
+        class="presentation__text"
+        data-aos="fade-right"
+        data-aos-duration="600"
+        data-aos-offset="300"
+        data-aos-delay="150"
+      >
         <p>
           <span>D</span>ynamique, motivé et appliqué, je touche à l'univers du
           Développement Web depuis maintenant plus d'un an.

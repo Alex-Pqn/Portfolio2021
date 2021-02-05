3c0347dbeb826bf76949fed28fc99a77cca3f122
Alexandre P <@paquienalexandre0@gmail.com>
2021-02-05 16:31:47 +0100

    import portfolio item component

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -7,14 +7,110 @@
       </h1>
     </div>
     <div class="portfolio__bottom">
-      <div v-for="item in items" :key="item.id"></div>
+      <div v-for="item in items" :key="item.id">
+        <portfolioItemComponent
+          :id="item.id"
+          :title="item.title"
+          :img="item.img"
+          :description="item.description"
+          :technologies="item.technologies"
+          :githubUrl="item.githubUrl"
+          :siteUrl="item.siteUrl"
+          maxItems="6"
+        />
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import portfolioItemComponent from "@/components/Portfolio/item.vue";
+
 export default {
-  name: "portfolio"
+  name: "portfolio",
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "Mon CV 1",
+          img: "CV.png",
+          description:
+            "Le projassrooms consistait à créer son propre CV sous foeb à l'aide du HTML et CSS seulement.",
+          technologies: {
+            HTMLCSS: true
+          },
+          githubUrl: "https://github.com/Alex-Pqn?tab=repositories",
+          siteUrl: "https://github.com/Alex-Pqn?tab=repositories"
+        },
+        {
+          id: 2,
+          title: "Mon CV 2",
+          img: "CV.png",
+          description:
+            "sistait à créer son propre CV sous forme d'une page web à l'a HTML et CSS seulement.",
+          technologies: {
+            HTMLCSS: true,
+            JS: true
+          },
+          githubUrl: "https://github.com/Alex-Pqn?tab=repositories",
+          siteUrl: "https://github.com/Alex-Pqn?tab=repositories"
+        },
+        {
+          id: 3,
+          title: "Mon CV 3",
+          img: "CV.png",
+          description:
+            "Le projet 2 Openms consistait à créer son proprpage web à l'aide du HTML et CSS seulement.",
+          technologies: {
+            HTMLCSS: true,
+            BOOTSTRAP: true
+          },
+          githubUrl: "https://github.com/Alex-Pqn?tab=repositories",
+          siteUrl: "https://github.com/Alex-Pqn?tab=repositories"
+        },
+        {
+          id: 4,
+          title: "Mon CV 4",
+          img: "CV.png",
+          description:
+            "Le projet à créer son propre CV sous forme d'une page web à l'aide du HTML et CSS seulement.",
+          technologies: {
+            VUEJS: true
+          },
+          githubUrl: "https://github.com/Alex-Pqn?tab=repositories",
+          siteUrl: "https://github.com/Alex-Pqn?tab=repositories"
+        },
+        {
+          id: 5,
+          title: "Mon CV 5",
+          img: "CV.png",
+          description:
+            "Le projet 2 OpenClassrooms consiste d'une page web à l'aide du HTML et CSS seulement.",
+          technologies: {
+            HTMLCSS: true
+          },
+          githubUrl: "https://github.com/Alex-Pqn?tab=repositories",
+          siteUrl: "https://github.com/Alex-Pqn?tab=repositories"
+        },
+        {
+          id: 6,
+          title: "Mon CV 6",
+          img: "CV.png",
+          description:
+            "Le projet 2 OpenClassrooms consistait à créer son propre CV sous form",
+          technologies: {
+            JS: true
+          },
+          githubUrl: "https://github.com/Alex-Pqn?tab=repositories",
+          siteUrl: "https://github.com/Alex-Pqn?tab=repositories"
+        }
+      ]
+    };
+  },
+  components: {
+    portfolioItemComponent
+  }
 };
 </script>
 

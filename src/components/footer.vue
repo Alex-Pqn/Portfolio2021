e07f51d4e77d8a2c6bcb7a1e8f7b9af04c80056c
Alexandre P <@paquienalexandre0@gmail.com>
2021-03-07 21:03:44 +0100

    initialize footer part

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer"></div>
+</template>
+
+<script>
+export default {
+  name: "footer"
+};
+</script>
+
+<style scoped lang="scss">
+.footer {
+  height: 20vh;
+  background-color: rgba(0, 0, 0, 0.2);
+}
+</style>
